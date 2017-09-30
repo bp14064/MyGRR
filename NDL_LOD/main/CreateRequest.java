e20f2209c7bb133f20e5d9b9454dac71f93ea23d
ShingoSuzaki <eclair.1008@gmail.com>
2017-09-30 23:26:57 +0900

    NDLのデータを使うための本格的なプログラム作成に向けての準備

---
+++ NDL_LOD/main/CreateRequest.java
@@ -0,0 +1,9 @@
+package main;
+
+public class CreateRequest {
+
+	public CreateRequest() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+}
---
+++ NDL_LOD/main/XMLAnalyzer.java
@@ -0,0 +1,9 @@
+package main;
+
+public class XMLAnalyzer {
+
+	public XMLAnalyzer() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+}
