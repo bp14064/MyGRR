fb356da5aa23568f4be821667977c05f2b6aaf4c
ShingoSuzaki <eclair.1008@gmail.com>
2017-10-01 14:46:01 +0900

    change XMLAnalyzer

--- NDL_LOD/main/XMLAnalyzer.java
+++ NDL_LOD/main/XMLAnalyzer.java
@@ -1,9 +1,26 @@
 package main;
 
-public class XMLAnalyzer {
+import java.io.File;
 
-	public XMLAnalyzer() {
-		// TODO 自動生成されたコンストラクター・スタブ
+public class XMLAnalyzer {
+	private String result = "";
+	public XMLAnalyzer(String result) {
+		this.result = result;
+	}
+
+	public void xmlAnalyze(String targetData) {
+		//一時保存用ファイルの作成
+		File filetmp = new File("C:\\Users\\Shingo\\git\\MyGRR\\NDL_LOD\\main\\tmp.xml");
+
+		if(targetData.matches("book")) {//書誌データの解析
+
+		}else if(targetData.matches("auth")) {//典拠データの解析
+
+		}
+
+		//一時保存用ファイルの削除
+		if(filetmp.delete())
+	    	System.out.println("xml file deleted");
 	}
 
 }
