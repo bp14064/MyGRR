6bc7cde631b607a896cdeaa7d98eef9bd8b2ee03
bp14064 <bp14064@shibaura-it.ac.jp>
2017-10-05 17:28:18 +0900

    change dcndl_test

--- NDL_LOD/dcndl_test/DcndlTest.java
+++ NDL_LOD/dcndl_test/DcndlTest.java
@@ -1,13 +1,6 @@
 package dcndl_test;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.Iterator;
 
 import org.dom4j.Document;
@@ -24,14 +17,14 @@ public class DcndlTest {
 		//String request = "http://iss.ndl.go.jp/api/sru?operation=searchRetrieve&query=title%3d%22%e3%81%93%e3%81%93%e3%82%8d%22%20AND%20creator%3d%22%e5%a4%8f%e7%9b%ae%e6%bc%b1%e7%9f%b3%22%20AND%20from%3d%222011%22%20AND%20until%3d%222013%22&recordSchema=dcndl_simple";
 		String request = "http://iss.ndl.go.jp/api/sru?operation=searchRetrieve&query=dpid%3D%22iss-ndl-opac%22+AND+title%3D%22%E3%81%93%E3%81%93%E3%82%8D%22+AND+creator%3D%22%E5%A4%8F%E7%9B%AE%E6%BC%B1%E7%9F%B3%22+AND+from%3D%222011%22+AND+until%3D%222013%22&recordSchema=dcndl&onlyBib";
 		String tmp="";
-		try {
+		/*try {
 		      URL url = new URL(request);
 		      URLConnection con = url.openConnection();
 		      try (BufferedReader reader = new BufferedReader(
 		        new InputStreamReader(con.getInputStream(), "UTF-8"))) {
 		    	  while (reader.ready()) {
 		    	    //System.out.println(reader.readLine());
-		    		  tmp+=reader.readLine();
+		    		  tmp+=reader.readLine()+"\n";
 		    	  }
 		      }
 		}catch (Exception e) {
@@ -47,7 +40,7 @@ public class DcndlTest {
 		} catch (IOException e1) {
 			// TODO 自動生成された catch ブロック
 			e1.printStackTrace();
-		}
+		}*/
 
 		//File file = new File("C:\\Users\\Shingo\\git\\MyGRR\\NDL_LOD\\dcdnl_test\\tmp.xml");
 		 File file = new File("C:\\Users\\AILab08\\git\\MyGRR\\NDL_LOD\\dcndl_test\\tmp.xml");
@@ -56,15 +49,27 @@ public class DcndlTest {
 		System.out.println("取得対象：タイトル、著者名、出版社、ページ数、ISBN、NDLC（請求記号）、NDC9、資料種別");
 
 		SAXReader reader = new SAXReader();
+		String records ="";
 	    try {
 	      Document doc = reader.read(file);
 	      Element root = doc.getRootElement();
-	      Element element;
+	      Element element = null;
 	      for (Iterator i = root.elementIterator(); i.hasNext();) {
 	    	  element = (Element) i.next();
-	    	  System.out.println(element.getName()+" "+element.getNodeTypeName()+" "+element.nodeCount());
-			  System.out.println("テキストを取り出す:" + element.getStringValue());
+	    	  //System.out.println(element.getName()+" "+element.getNodeTypeName()+" "+element.nodeCount());
+			  //System.out.println("テキストを取り出す:" + element.getStringValue());
+	    	  if(element.getName().matches("records")) {
+	    		 records = element.getStringValue();
+	    		 break;
+	    	  }
 	      }
+
+	      //recordsをテキストファイルに一回保存
+	      File recordsFile = new File("");
+	      //ゴリ押しのテキスト処理
+
+
+
 
 
 	    }catch (DocumentException e) {
