15917967aa74bbd388c3f92fbbdad465585cec2e
bp14064 <bp14064@shibaura-it.ac.jp>
2017-10-16 17:07:41 +0900

    change kep

--- Prototype/kep/KeywordExplorerProto.java
+++ Prototype/kep/KeywordExplorerProto.java
@@ -28,13 +28,10 @@ public class KeywordExplorerProto {
 		ArrayList<ArrayList<String>> res = kep.getSubjectData(keyword);
 		kep.checkSubjectDataResult(res);
 
-		/*
-		 * for (String s : b) System.out.println("上位語：" + s); for (String s : n)
-		 * System.out.println("下位語：" + s); for (String s : r) System.out.println("関連語："
-		 * + s); for (String s : c) System.out.println("NDC：" + s);
-		 */
-		// NDCは、この後にNDC部分を取り出す処理を行う リストのリスト？
+		// NDCのものは、その代表分類に対応する件名を取得する
+		kep.checkSubjectDataResult(res, true);
 
+		// NDCは、この後にNDC部分を取り出す処理を行う リストのリスト？
 		// 取った上位語、下位語、関連語でさらに検索をかけ、その分類を取得する
 
 	}
@@ -68,22 +65,62 @@ public class KeywordExplorerProto {
 		return result;
 	}
 
+	private ArrayList<String> getNDCData(String target) {
+		CreateRequest cr = new CreateRequest(); // リクエスト作成
+		RequestTR rtr = new RequestTR(); // リクエスト送受信
+		ResultAnalyzer ra = new ResultAnalyzer(); // 結果解析
+		ArrayList<String> result = new ArrayList<String>();
+
+		try {
+			result = ra.AnalyzeResult(rtr.requestProcess(cr.createRequest(target, "non", "relatedMatch")));
+		} catch (ArgsTypeException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+
 	private void checkSubjectDataResult(ArrayList<ArrayList<String>> result) {
 		ArrayList<String> tmp = null;
 		String[] subType = null;
-		if(result.size() == 4) {
-		   subType = new String[4];
-		   subType[0] = "上位語";
-		   subType[1] = "下位語";
-		   subType[2] = "関連語";
-		   subType[3] = "代表分類";
+		if (result.size() == 4) {
+			subType = new String[4];
+			subType[0] = "上位語";
+			subType[1] = "下位語";
+			subType[2] = "関連語";
+			subType[3] = "代表分類";
 		}
 
 		for (int i = 0; i < result.size(); i++) {
 			tmp = result.get(i);
 			for (String res : tmp) {
-				if(result.size() == 4)
+				if (result.size() == 4)
 					System.out.println(subType[i] + ":" + res);
+			}
+		}
+	}
+
+	private void checkSubjectDataResult(ArrayList<ArrayList<String>> result, boolean relatedMatch) {
+		ArrayList<String> tmp = null;
+		ArrayList<ArrayList<String>> resNdc = new ArrayList<ArrayList<String>>();
+		ArrayList<String> ndc = result.get(3);
+		ArrayList<String> ndcLabel = new ArrayList<String>();
+
+		System.out.println("");
+		System.out.println("代表分類に対応する件名");
+		for (String l : ndc) {
+			if (l.contains("ndc9")) {
+				ndcLabel.add(l.split("/")[1]);
+				resNdc.add(this.getNDCData(l));
+			}
+		}
+
+		for (int i = 0; i < resNdc.size(); i++) {
+			String n = ndcLabel.get(i);
+			tmp = resNdc.get(i);
+			for (String r : tmp) {
+				System.out.println("代表分類 " + n + " : " + r);
 			}
 		}
 	}
