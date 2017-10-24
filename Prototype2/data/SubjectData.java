f7b391750db9df014507f3bc80e8a8cea9f83cca
ShingoSuzaki <eclair.1008@gmail.com>
2017-10-24 20:23:35 +0900

    change master

--- Prototype2/data/SubjectData.java
+++ Prototype2/data/SubjectData.java
@@ -4,14 +4,33 @@ import java.util.ArrayList;
 
 public class SubjectData {
 	private String subject;
+	//この件名の代表分類
 	private ArrayList<String> ndc;
 	private ArrayList<String> ndc9;
 	private ArrayList<String> ndc10;
+	//上位語
+	private ArrayList<String> broader;
+	//下位語
+	private ArrayList<String> narrower;
+	//関連語
+	private ArrayList<String> related;
+	//この件名が持つ代表分類に属する件名
+	private ArrayList<String> sameCategorySubject;
 
 	public SubjectData(String subject, ArrayList<String> ndc) {
 		this.subject = subject;
 		this.ndc = ndc;
 		this.inputNDC(ndc);
+	}
+
+	public SubjectData(String subject, ArrayList<String> ndc, ArrayList<String> broader, ArrayList<String> narrower, ArrayList<String> related, ArrayList<String> sameCategorySubject) {
+		this.subject = subject;
+		this.ndc = ndc;
+		this.inputNDC(ndc);
+		this.broader = broader;
+		this.narrower = narrower;
+		this.related = related;
+		this.sameCategorySubject = sameCategorySubject;
 	}
 
 	private void inputNDC(ArrayList<String> target) {
@@ -32,16 +51,44 @@ public class SubjectData {
 	}
 
 	public void showSubjectData() {
-		System.out.print("件名 : " + this.subject);
-		System.out.print(" : NDC9 ");
+		System.out.println("件名 : " + this.subject);
+		System.out.print("NDC9: ");
 		for(String n9 : this.ndc9) {
 			System.out.print(n9 + " / ");
 		}
-		System.out.print(" : NDC10 ");
+		System.out.print(" NDC10: ");
 		for(String n10 : this.ndc10) {
 			System.out.print(n10 + " / ");
 		}
 		System.out.println("");
+		if(!this.broader.isEmpty()) {
+			System.out.print("上位語:");
+			for(String b : this.broader) {
+				System.out.print(b + " / ");
+			}
+			System.out.println("");
+		}
+		if(!this.narrower.isEmpty()) {
+			System.out.print("下位語:");
+			for(String n : this.narrower) {
+				System.out.print(n + " / ");
+			}
+			System.out.println("");
+		}
+		if(!this.related.isEmpty()) {
+			System.out.print("関連語:");
+			for(String r : this.related) {
+				System.out.print(r + " / ");
+			}
+			System.out.println("");
+		}
+		if(!this.sameCategorySubject.isEmpty()) {
+			System.out.print("同じ代表分類に属する件名:");
+			for(String s : this.sameCategorySubject) {
+				System.out.print(s + " / ");
+			}
+			System.out.println("");
+		}
 	}
 
 	public String getSubject() {
@@ -58,6 +105,22 @@ public class SubjectData {
 
 	public ArrayList<String> getNdc10() {
 		return ndc10;
+	}
+
+	public ArrayList<String> getBroader() {
+		return broader;
+	}
+
+	public ArrayList<String> getNarrower() {
+		return narrower;
+	}
+
+	public ArrayList<String> getRelated() {
+		return related;
+	}
+
+	public ArrayList<String> getSameCategorySubject() {
+		return sameCategorySubject;
 	}
 
 }
